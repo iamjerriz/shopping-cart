7bc7e60dca31f5e9401da99f5858897e429e2999
Jerriz Evangelista <iamjerriz@gmail.com>
2022-10-24 13:05:10 +0800

    xzc

--- shopping-cart/example_usage/jerrizshoppingcart/src/App.js
+++ shopping-cart/example_usage/jerrizshoppingcart/src/App.js
@@ -1,29 +1,50 @@
 import './App.css';
 import React, { useState } from "react";
-import CartComponent from 'shopping-cart-jerriz-v1';
+import { CartComponent } from 'riz-shoppingcartasd-v2';
 
 function App() {
 
-   const [products, setCart] = useState([ 
-    { id: 1, name: "Beer", price: 1.00, quantity: 0, imgUrl: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/57089256%20-%2001.jpg"},
-    { id: 2, name: "Vinegar", price: 21.00, quantity: 0, imgUrl: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4806515630291-1.jpg"},
-    { id: 3, name: "Pork & Beans", price: 20.00, quantity: 0, imgUrl: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/119593-01.jpg"},
-    { id: 4, name: "Blanca Mix", price: 10.00, quantity: 0, imgUrl: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4800552169066-01.jpg"}
-    ]);
+  const [products, setCart] = useState([
+    { id: 1, name: "Beer", price: 1.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/57089256%20-%2001.jpg" },
+    { id: 2, name: "Vinegar", price: 21.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4806515630291-1.jpg" },
+    { id: 3, name: "Pork & Beans", price: 20.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/119593-01.jpg" },
+    { id: 4, name: "Blanca Mix", price: 10.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4800552169066-01.jpg" }
+  ]);
 
-  const remove = (id) => {
-    let newArr = products.filter(products => products.id !== id);
-    setCart(newArr);
-    console.log("asd",newArr)
+  // const items = [
+  //   { id: 1, name: "Beer", price: 1.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/57089256%20-%2001.jpg" },
+  //   { id: 2, name: "Vinegar", price: 21.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4806515630291-1.jpg" },
+  //   { id: 3, name: "Pork & Beans", price: 20.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/119593-01.jpg" },
+  //   { id: 4, name: "Blanca Mix", price: 10.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4800552169066-01.jpg" },
+  //   { id: 4, name: "Blanca Mix", price: 10.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4800552169066-01.jpg" },
+  //   { id: 4, name: "Blanca Mix", price: 10.00, quantity: 0, img: "https://qa-centralmain.s3.ap-southeast-1.amazonaws.com/market/4800552169066-01.jpg" }
+  // ]
+
+  const btnFunc1 = () => {
+    console.log("btn1 Clicked")
   }
+
+  const btnFunc2 = () => {
+    console.log("btn2 Clicked")
+  }
+
+  // const remove = (id) => {
+  //   let newArr = products.filter(products => products.id !== id);
+  //   setCart(newArr);
+  //   console.log("asd", newArr)
+  // }
 
   return (
     <div className="App">
-      <CartComponent 
-        items={[products]} 
-        title={"Jerriz Store"} 
-        showRemove={true} 
-        removeProdFunc={remove} />
+      <CartComponent
+        items={[products]}
+        btnFunction1={btnFunc1}
+        btnFunction2={btnFunc2}
+        btnText1={"+"}
+        btnText2={"-"}
+      />
     </div>
   );
 }
+
+export default App
--- shopping-cart/src/views/CartView.tsx
+++ shopping-cart/src/views/CartView.tsx
@@ -13,45 +13,39 @@ interface Props {
 
 export const CartView = ({ items, btnFunction1, btnFunction2, btnText1, btnText2 }: Props) => {
 
-    useState([{ items }]);
-    console.log("mini-app-items", items)
-
+    console.log("miniapp-items", items)
     return (
-        <div className="text-center w-100">
-            <h1>Shopping Cart</h1>
-            <div className="d-flex flex-wrap w-100">
-                {
-                    items[0].map((product: itemsState) =>
-                        <Card
-                            className="d-flex bg-white shadow-sm m-2 col-2 p-1"
-                            style={{ minWidth: "30%" }}
-                            key={product.id}>
-                            <Card.Img
-                                className="p-1"
-                                variant="top"
-                                src={product.img}
-                                height="100px"
-                                width="100px"
-                                style={{ objectFit: "contain" }}
-                            />
-                            <Card.Body className="d-flex flex-column p-1">
+        <div className="d-flex flex-wrap w-100">
+            {
+                items[0][0][0].map((product: itemsState) =>
+                    <Card
+                        className="d-flex bg-white shadow-sm m-2 col-2 p-1"
+                        style={{ minWidth: "30%" }}
+                        key={product.id}>
+                        <Card.Img
+                            className="p-1"
+                            variant="top"
+                            src={product.img}
+                            height="100px"
+                            width="100px"
+                            style={{ objectFit: "contain" }}
+                        />
+                        <Card.Body className="d-flex flex-column p-1">
 
-                                <Card.Title className="d-flex flex-row justify-content-between my-3">
-                                    <span className="fs-.5">{product.name}</span>
-                                    <span className="ms-2 text-muted">₱ {product.price}</span>
-                                </Card.Title>
+                            <Card.Title className="d-flex flex-row justify-content-between my-3">
+                                <span className="fs-.5">{product.name}</span>
+                                <span className="ms-2 text-muted">₱ {product.price}</span>
+                            </Card.Title>
 
-                                <div className="d-flex flex-row justify-content-between">
-                                    <CustomButton text={btnText1} onClickFunction={btnFunction1} param={undefined} />
-                                    <span className="ml-4 mr-4 cart ">{product.quantity}</span>
-                                    <CustomButton text={btnText2} onClickFunction={btnFunction2} param={undefined} />
-
-                                </div>
-                            </Card.Body >
-                        </Card>
-                    )
-                }
-            </div>
+                            <div className="d-flex flex-row justify-content-between">
+                                <CustomButton text={btnText1} onClickFunction={btnFunction1} param={product} />
+                                <span className="ml-4 mr-4 cart ">{product.quantity}</span>
+                                <CustomButton text={btnText2} onClickFunction={btnFunction2} param={product} />
+                            </div>
+                        </Card.Body >
+                    </Card>
+                )
+            }
         </div>
     )
 }
